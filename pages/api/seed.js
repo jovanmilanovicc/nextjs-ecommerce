6044dca73765f5b80456c0686464d705dc8f39bf
Jodzav2 <105559220+Jodzirv2@users.noreply.github.com>
2023-05-06 16:47:22 +0200

    Seeder for users

--- pages/api/seed.js
+++ pages/api/seed.js
@@ -1,4 +1,5 @@
 import Product from "@/models/Products";
+import User from "@/models/User";
 import data from "@/utils/data";
 import db from "@/utils/db";
 import nextConnect from "next-connect";
@@ -8,7 +9,10 @@ const handler = nextConnect();
 handler.get(async (req, res) => {
     await db.connect();
     await Product.deleteMany();
-    await Product.insertMany(data.products)
+    await Product.insertMany(data.products);
+
+    await User.deleteMany();
+    await User.insertMany(data.users)
     await db.disconnect();
     res.send({message: 'Seeded ok'})
 });
